ed56d5ed88dee39a536bbdda64d42524cc35bca9
abcd-EGH <wlghks7790@gmail.com>
2024-11-14 01:54:48 +0900

    [CHORE] class list in __init__.py

--- __init__.py
+++ __init__.py
@@ -7,4 +7,4 @@ from .sgru_autoencoder import SGRUAutoEncoder
 __version__ = "0.1.0" # 버전 업데이트 시 변경
 
 # 외부에서 접근 가능한 클래스 목록 정의
-__all__ = ["slstm_autoencoder", "SGRUAutoEncoder"] # 외부에서 접근 가능한 클래스 목록
+__all__ = ["SLSTMAutoEncoder", "SGRUAutoEncoder"] # 외부에서 접근 가능한 클래스 목록

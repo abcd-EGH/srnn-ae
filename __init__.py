577da488887aff2149b933c88f788edd2249b714
abcd-EGH <wlghks7790@gmail.com>
2024-11-14 01:52:53 +0900

    [MODIFY] package name and license, author-email

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 # SLSTMAutoEncoder와 SGRUAutoEncoder 모듈의 주요 클래스를 임포트
-from .SLSTMAutoEncoder import SLSTMAutoEncoder
-from .SGRUAutoEncoder import SGRUAutoEncoder
+from .slstm_autoencoder import SLSTMAutoEncoder
+from .sgru_autoencoder import SGRUAutoEncoder
 # from .SLSTMVAE import SLSTMVAE # 새로운 클래스 추가 시 임포트
 
 # 패키지 버전 정보 (선택 사항)
 __version__ = "0.1.0" # 버전 업데이트 시 변경
 
 # 외부에서 접근 가능한 클래스 목록 정의
-__all__ = ["SLSTMAutoEncoder", "SGRUAutoEncoder"] # 외부에서 접근 가능한 클래스 목록
+__all__ = ["slstm_autoencoder", "SGRUAutoEncoder"] # 외부에서 접근 가능한 클래스 목록
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="SRNNAutoEncoder",  # 패키지 이름
+    name="srnn-ae",  # 패키지 이름
     version="0.1.0", # 버전 업데이트 시 변경
     packages=find_packages(),
     install_requires=[  # 필요한 라이브러리 목록
@@ -9,6 +9,8 @@ setup(
         "torch",
     ],
     author="Jihwan Lee (abcd-EGH)",
+    author_email="wlghks7790@gmail.com",
+    license="MIT",
     description="Sparcely connections RNN + AutoEncoder Model for Anomaly Detection in Time Series",
     url="https://github.com/abcd-EGH/srnn-ae",
 )

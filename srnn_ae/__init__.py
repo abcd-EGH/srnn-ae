ed94d395c8ff979c18493b8c8023b7c8112ad9a6
abcd-EGH <wlghks7790@gmail.com>
2024-11-15 17:53:37 +0900

    [CHORE] remove __version__ in __init__.py

--- srnn_ae/__init__.py
+++ srnn_ae/__init__.py
@@ -2,8 +2,5 @@
 from .slstm_autoencoder import SLSTMAutoEncoder, set_random_seed
 # from .SLSTMVAE import SLSTMVAE # 새로운 클래스 추가 시 임포트
 
-# 패키지 버전 정보 (선택 사항)
-__version__ = "0.1.1" # 버전 업데이트 시 변경
-
 # 외부에서 접근 가능한 클래스, 함수 목록 정의
 __all__ = ["SLSTMAutoEncoder", "set_random_seed"] # 외부에서 접근 가능한 클래스, 함수 목록

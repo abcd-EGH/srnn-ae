6b9b2ca8502d11f2eef82b80258251fc0f5ad192
abcd-EGH <wlghks7790@gmail.com>
2024-11-21 20:12:42 +0900

    [CHORE] __all__ in __init__.py

--- srnn_ae/__init__.py
+++ srnn_ae/__init__.py
@@ -4,4 +4,4 @@ from .utils import set_random_seed, hyperparameter_setting, read_dataset
 # from .SLSTMVAE import SLSTMVAE # 새로운 클래스 추가 시 임포트
 
 # 외부에서 접근 가능한 클래스, 함수 목록 정의
-__all__ = ["SLAE", "set_random_seed", "hyperparameter_setting", "read_dataset"] # 외부에서 접근 가능한 클래스, 함수 목록
+__all__ = ["SLAEE", "set_random_seed", "hyperparameter_setting", "read_dataset"] # 외부에서 접근 가능한 클래스, 함수 목록
